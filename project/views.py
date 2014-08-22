7092c07823bf8a1965b8f61300ae82e87bbd99e4
phillyc <services@philcarter.me>
2014-08-22 17:57:46 -0400

    Remove tags.

--- project/views.py
+++ project/views.py
@@ -24,13 +24,13 @@ def page(path):
     return render_template('page.html', page=page)
 
 
-@app.route('/tags/<tag>')
-def tag(tag):
-    # Get all published pages.
-    published_pages = [page for page in pages if 'date' in page.meta]
+# @app.route('/tags/<tag>')
+# def tag(tag):
+#     # Get all published pages.
+#     published_pages = [page for page in pages if 'date' in page.meta]
 
-    # List of pages with the given tag.
-    pages_with_tag = [page for page in published_pages if tag in page.meta['tags']]
+#     # List of pages with the given tag.
+#     pages_with_tag = [page for page in published_pages if tag in page.meta['tags']]
 
-    return render_template('index.html', pages=pages_with_tag)
+#     return render_template('index.html', pages=pages_with_tag)
 
